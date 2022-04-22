9ff205b45038b082e451921c9a19074df3918bea
ykoitzsch <y.koitzsch@gmx.at>
2022-04-22 10:37:34 +0200

    setup

---
+++ src/main/java/network/gitter/executor/gitterexecutorbot/GitterExecutorBotApplication.java
@@ -0,0 +1,13 @@
+package network.gitter.executor.gitterexecutorbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitterExecutorBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitterExecutorBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/network/gitter/executor/gitterexecutorbot/GitterExecutorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package network.gitter.executor.gitterexecutorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitterExecutorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
